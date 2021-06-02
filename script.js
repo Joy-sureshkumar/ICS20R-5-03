592ac703a0656c5c0845b59e8ae4d7ed55bbba91
Joy Sureshkumar <>
2021-06-02 14:08:49 +0000

    changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('report').addEventListener('click', reportResults)
 
-function reportResults() {
+function reportResults () {
   alert(document.getElementById('input').value)
 }
